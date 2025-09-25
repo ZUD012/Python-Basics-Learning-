8f17c573ee35bd4dacbce8cc1c49380de3113066
ZUD012 <pathdevlal11@gmail.com>
2025-09-25 17:28:31 +0530

    Understanding concepts of string

---
+++ python2.py
@@ -0,0 +1,17 @@
+# Working in strinngs -->
+a =  input("Enter Your Name : ")
+
+print("The name of user is : ",a)
+
+STR = "i AM GOING TO MASTER AI AND MACHINE LEARNING --> "
+
+print(STR.endswith("ING")) # checks if the string ends with ING .
+
+STR = STR.capitalize() #capitalise the first element of your string . 
+
+print(STR.replace("MASTER" , "BECOME MASTER" ))
+
+print(STR)
+
+print(STR.find("am"))
+print(STR.count("am"))
