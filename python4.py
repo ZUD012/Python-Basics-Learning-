c07ad61bb7af8ca9131bcd3d580374096370f1d5
ZUD012 <pathdevlal11@gmail.com>
2025-09-25 17:43:06 +0530

    Tuples in Python

---
+++ python4.py
@@ -0,0 +1,10 @@
+# Tuples (Immutable otherwise same as lists)
+tup = (87 ,99,55,67)
+tup1 = ("Ramesh" , "Satnam" , "Ramesh")
+print(tup)
+print(tup[3]) # Accesing element at paticular index . 
+
+print(tup.index(99)) # Find the index of an element . 
+
+tup.count("Ramesh") # Count how many times an element is present in the tuple . 
+
