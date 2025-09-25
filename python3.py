e104231228b92808d97986d312a44c7d69be3187
ZUD012 <pathdevlal11@gmail.com>
2025-09-25 17:37:27 +0530

    Working with Lists

---
+++ python3.py
@@ -0,0 +1,23 @@
+marks = [1, "Riya" ,"Kamal"]
+
+lists = [1,2,3,4,77,6,88,2]
+
+print(marks)
+
+print(type(marks))
+print(marks[0])
+
+marks.append(5)
+print(marks)
+
+lists.sort()
+print (lists)
+
+lists.sort(reverse=True)
+print (lists)
+
+lists.reverse()
+print(lists)
+
+lists.insert(2,"Kamal")
+print(lists)
