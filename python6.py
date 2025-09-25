5cd3ae616f201b4987bb3b9181e919e0840c926f
ZUD012 <pathdevlal11@gmail.com>
2025-09-25 18:02:07 +0530

    Understanding Sets in Python

---
+++ python6.py
@@ -0,0 +1,23 @@
+# Sets -->
+
+sets = {1,5,7,"naman" , "Kapil" , "ZUDO"}
+set2 = {1,5,7,8,9,33}
+print(sets)
+
+sets.add(9)  # Adding an element in set .
+print(sets)
+
+sets.remove(5) # Removing apaticular element from the set .
+print(sets)
+
+sets.pop()
+print(sets) # Remove Random element from set . 
+
+sets.union(set2) # Adding two sets .
+print(sets)
+
+sets.intersection(set2) # Taking Common elements from two sets .
+print(sets)
+
+sets.clear() # returning a empty set . 
+print(sets)
