1199b6cc2096563542bee744aa0e5292cdaf3e92
ZUD012 <pathdevlal11@gmail.com>
2025-09-28 03:48:32 +0530

    Working on Binary Files

--- python10.py
+++ python10.py
@@ -1,14 +1,6 @@
-with open("Practice_file_I-O.txt", "r") as f:
-    data = f.read()   
-new_data = data.replace("Parth" , "Zudo")
-print ( new_data)
+import pickle
 
-with open("Practice_file_I-O.txt","w") as f :
-    f.write(new_data)
+with open ("D:\Python Basics Coding\practice.pickle" , "rb") as f :
+    a = pickle.load(f)
+    print(a)
 
-    with open("Practice_file_I-O.txt", "r") as f:
-         data1 = f.read()   
-         if(data1.find("which")):
-             print("The word exist inside the file : ")
-         else :
-             print("No similar elemnt found : ")
